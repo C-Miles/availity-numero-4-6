25e929bbc3912262b5e7ec172776d14cdb4d1e13
Chris Miles <cw.miles121@gmail.com>
2022-01-17 10:11:02 -0500

    clarifying comments

--- AvailityAssessments/src/Numero4/LispReader.java
+++ AvailityAssessments/src/Numero4/LispReader.java
@@ -4,15 +4,19 @@ public class LispReader {
 	
 	boolean validator(String str) {
 		
+//		Turn input into an array for looping
 		char[] arr = str.toCharArray();
+		
+//		Copy first array into a reference type array to utilize .equals
 		Character[] arr2 = new Character[arr.length];
 		
 		for(int i = 0; i < arr.length; i++) {
 			arr2[i] = arr[i];
 		}
 		
-		int count = 0;
+		int count = 0; 
 		
+//		Increment/decrement counter to keep track of parentheses, return false if ever negative or anything other than 0
 		for(int i = 0; i < arr2.length; i++) {
 			if(arr2[i].equals('(')) {
 				count += 1;
